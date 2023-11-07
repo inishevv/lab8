6c5a1050046713fe755a7d08b74d7b1c9d952fe4
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-07 22:50:59 +0500

    Create taxi.py
    Создал файл, ввод количества сотрудников, проверка на число

---
+++ taxi.py
@@ -0,0 +1,11 @@
+﻿def get_number_input(prompt):#для проверки является ли введеное значение числом
+    while True:
+        user_input = input(prompt)
+        try:
+            number = float(user_input)
+            return number
+        except ValueError:
+            print("Введено не число, попробуйте снова.")
+
+print('Введите количество сотрудников:')
+n = get_number_input('')
