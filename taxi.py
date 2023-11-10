304555e76eec4d9dd18522fc39a1d82520813e37
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-10 11:21:38 +0500

    расчет наилучшего выбора такси

--- taxi.py
+++ taxi.py
@@ -1,4 +1,4 @@
-﻿def get_number_input(prompt):#для проверки является ли введеное значение числом
+def get_number_input(prompt): #для проверки является ли введеное значение числом
     while True:
         user_input = input(prompt)
         try:
@@ -9,3 +9,26 @@
 
 print('Введите количество сотрудников:')
 n = get_number_input('')
+print('Введите расстояния в километрах до домов сотрудников от дома:')
+km = input()
+mas_km = []
+print('Введите стоимость такси за километр')
+cell = input()
+mas_cell= []
+proverka = mas_cell
+km = km.split()
+for i in km:
+    mas_km.append(int(i))
+cell = cell.split()
+for j in cell:
+    mas_cell.append(int(j))
+print(mas_km, mas_cell)
+
+mas_cell.sort()
+mas_km.sort(reverse=True)
+
+print(mas_km, mas_cell)
+for q in range(len(mas_km)):
+    res = mas_km[q] * mas_cell[q]
+    print(res)
+print(proverka)
